6cb6332697f4fcce9b3d8ddc824c04dfc0a969dd
nastya7007 <b.nastya7007@yandex.ru>
2026-09-23 12:57:34 +0700

    добавлен класс enemyicon

---
+++ EnemyIcon.cs
@@ -0,0 +1,14 @@
+﻿uusing System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnemyEditor
+{
+    public class EnemyIcon
+    {
+        public string Name { get; set; }
+        public string ImagePath { get; set; }
+    }
+}
