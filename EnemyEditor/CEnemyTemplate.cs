4aa495d11e2e914223efd6ace16e110bfd42300e
nastya7007 <b.nastya7007@yandex.ru>
2026-09-23 12:33:14 +0700

    ссососсоооздаан класссасас cenemytemplate

---
+++ EnemyEditor/CEnemyTemplate.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnemyEditor
+{
+    public class CEnemyTemplate
+    {
+        public string Name { get; private set; }
+        public string IconName { get; private set; }
+        public int BaseLife { get; private set; }
+        public double LifeModifier { get; private set; }
+        public int BaseGold { get; private set; }
+        public double GoldModifier { get; private set; }
+        public double SpawnChance { get; private set; }
+
+        public CEnemyTemplate(string name, string iconName, int baseLife,
+            double lifeModifier, int baseGold,
+            double goldModifier, double spawnChance)
+        {
+            Name = name;
+            IconName = iconName;
+            BaseLife = baseLife;
+            LifeModifier = lifeModifier;
+            BaseGold = baseGold;
+            GoldModifier = goldModifier;
+            SpawnChance = spawnChance;
+        }
+    }
+}
