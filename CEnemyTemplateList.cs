b472d2d149a913ccd9f45ec60ba20d8dc0d5cabd
nastya7007 <b.nastya7007@yandex.ru>
2026-09-23 12:51:39 +0700

    добавлено сохрвание ии загрузка врагов вв jsooon

--- CEnemyTemplateList.cs
+++ CEnemyTemplateList.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace EnemyEditor
 {
     public class CEnemyTemplateList
     {
+        [JsonInclude]
         private List<CEnemyTemplate> enemies;
 
         public CEnemyTemplateList()
@@ -62,6 +66,37 @@ namespace EnemyEditor
                 names.Add(enemy.Name);
             }
             return names;
+        }
+
+        //JSON
+
+        public void SaveToJson(string path)
+        {
+            string json = JsonSerializer.Serialize(enemies);
+            File.WriteAllText(path, json);
+        }
+
+        public void LoadFromJson(string path)
+        {
+            string json = File.ReadAllText(path);
+            enemies = new List<CEnemyTemplate>();
+
+            JsonDocument doc = JsonDocument.Parse(json);
+
+            foreach (JsonElement element in doc.RootElement.EnumerateArray())
+            {
+                string name = element.GetProperty("Name").GetString();
+                string iconName = element.GetProperty("IconName").GetString();
+                int baseLife = element.GetProperty("BaseLife").GetInt32();
+                double lifeModifier = element.GetProperty("LifeModifier").GetDouble();
+                int baseGold = element.GetProperty("BaseGold").GetInt32();
+                double goldModifier = element.GetProperty("GoldModifier").GetDouble();
+                double spawnChance = element.GetProperty("SpawnChance").GetDouble();
+
+                CEnemyTemplate enemy = new CEnemyTemplate(name, iconName, baseLife,
+                    lifeModifier, baseGold, goldModifier, spawnChance);
+                enemies.Add(enemy);
+            }
         }
     }
 }
